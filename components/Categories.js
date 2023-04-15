b62c61b8ba1f41ba84c76083ef32a35fe4b0ede6
Oleksii Bardyshev <oleksii.bardyshev@unilimes.com>
2023-04-15 22:40:32 +0300

    FeaturedRow component was added

---
+++ components/Categories.js
@@ -0,0 +1,23 @@
+import React from "react";
+import {View, Text, ScrollView} from "react-native";
+import CategoryCard from "./CategoryCard";
+
+const Categories = () => {
+  return (
+    <ScrollView
+      horizontal
+      contentContainerStyle={{paddingHorizontal: 15, paddingTop: 10}}
+      showsHorizontalScrollIndicator={false}
+    >
+      {/*Category card*/}
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing" />
+      <CategoryCard imgUrl="https://links.papareact.com/gn7" title="Testing" />
+    </ScrollView>
+  );
+};
+
+export default Categories;
